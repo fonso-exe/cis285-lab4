fcbf0d6a3ef5f9fa7a55292559b84b917271a433
FONSO-DESKTOP <alfonsotorcuator@gmail.com>
2025-10-02 19:46:37 -0400

    added Jenkins print statement in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
     public static void main (String[] args) {
         for (int i = 1; i < 11; i++) {
+            //POST JENKINS UPDATE
             System.out.println("Iteration: " + i);
             System.out.println("Hello World...");
         }
+        System.out.println("Jenkins Test?");
     }
 }
