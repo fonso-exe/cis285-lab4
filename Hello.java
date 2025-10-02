a0b6e04087caedaea53695c517fb46c45a0e0a7b
FONSO-DESKTOP <alfonsotorcuator@gmail.com>
2025-10-02 19:11:38 -0400

    created 'feature' branch and edited the for loop

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main (String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello World..." + i);
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Iteration: " + i);
+            System.out.println("Hello World...");
         }
     }
 }
